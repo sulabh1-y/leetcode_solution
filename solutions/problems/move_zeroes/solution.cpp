8b8c32bb82a424e6ee6954f1ca2f431ce7481f7a
sulabh1-y <126059586+sulabh1-y@users.noreply.github.com>
2026-02-01 17:10:00 +0000

    Sync LeetCode submission - Move Zeroes (cpp)

---
+++ solutions/problems/move_zeroes/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) 
+    {
+        int n=nums.size();
+        int pos=0;
+        for(int i=0;i<n;i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[pos]=nums[i];
+                pos++;
+            }
+        }
+        while(pos<n)
+        {
+            nums[pos]=0;
+            pos++;
+        }
+        
+    }
+};
