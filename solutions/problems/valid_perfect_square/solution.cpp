a63d2d8162ae94822a55b8dc1f1b8ce688646571
sulabh1-y <126059586+sulabh1-y@users.noreply.github.com>
2026-06-20 15:35:02 +0000

    Sync LeetCode submission - Valid Perfect Square (cpp)

---
+++ solutions/problems/valid_perfect_square/solution.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool isPerfectSquare(int num) 
+    {
+        int root=sqrt(num);
+
+        if(root*root == num)
+        {
+            return true;
+        }
+        return false;
+    }
+};
