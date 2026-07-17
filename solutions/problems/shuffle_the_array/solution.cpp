c637d6dfab688e52374c401861ce433134ca9ca6
sulabh1-y <126059586+sulabh1-y@users.noreply.github.com>
2026-07-17 18:44:08 +0000

    Sync LeetCode submission - Shuffle the Array (cpp)

---
+++ solutions/problems/shuffle_the_array/solution.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> shuffle(vector<int>& nums, int n) 
+    {
+        vector<int> ans(2*n);
+        int y = n; 
+
+        for(int i = 0; i < 2*n; i++)
+        {
+            if(i % 2 == 0)
+            {
+                ans[i] = nums[i/2];  
+            }
+            else
+            {
+                ans[i] = nums[y++];  
+            }
+        }
+        return ans;
+    }
+};
