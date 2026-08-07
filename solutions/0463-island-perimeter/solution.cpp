1df5d12eaeca47b4e029c794e301387b0136e986
sulabh1-y <126059586+sulabh1-y@users.noreply.github.com>
2026-08-07 00:04:20 +0000

    Sync LeetCode submission Runtime - 3 ms (68.30%), Memory - 102.1 MB (43.59%)

---
+++ solutions/0463-island-perimeter/solution.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int islandPerimeter(vector<vector<int>>& grid) 
+    {
+        int row=grid.size();
+        int col=grid[0].size();
+        int per=0;
+        for(int i=0;i<row;i++)
+        {
+            for(int j=0;j<col;j++)
+            {
+                if(grid[i][j]==1)
+                {
+                    per+=4;
+                    if(i>0 && grid[i-1][j]==1)
+                    {
+                        per-=2;
+                    }
+                    if(j>0 && grid[i][j-1]==1)
+                    {
+                        per-=2;
+                    }
+                }
+            }
+        } 
+        return per;
+    }
+};
