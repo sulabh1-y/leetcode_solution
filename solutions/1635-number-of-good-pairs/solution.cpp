38bfef9afa3cd6435d30e30068fe97b8078540c4
sulabh1-y <126059586+sulabh1-y@users.noreply.github.com>
2026-08-08 13:36:36 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 9.8 MB (52.43%)

---
+++ solutions/1635-number-of-good-pairs/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numIdenticalPairs(vector<int>& nums) 
+    {
+        int goodp=0;
+        int n=nums.size();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=i+1;j<n;j++)
+            {
+                if(nums[i]==nums[j])
+                {
+                    goodp++;
+                }
+            }
+        }
+        return goodp;
+    }
+};
