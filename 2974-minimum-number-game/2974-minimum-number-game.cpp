145b901d10d3c56a61901946a781dd2522055c46
sulabh kumar sharma <126059586+sulabh1-y@users.noreply.github.com>
2026-08-11 05:05:03 +0530

    Time: 4 ms (10.7%), Space: 28.3 MB (29.38%) - LeetHub

--- 2974-minimum-number-game/2974-minimum-number-game.cpp
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -7,12 +7,13 @@ public:
         vector<int> arr;
         int alice,bob;
         sort(nums.begin(),nums.end());
-        for(int i=0;i<n-1;i+=2)
+        for(int i=0;i<n-1;i++)
         {
             alice=nums[i];
             bob=nums[i+1];
             arr.push_back(bob);
             arr.push_back(alice);
+            i++;
         }
         return arr;
     }
