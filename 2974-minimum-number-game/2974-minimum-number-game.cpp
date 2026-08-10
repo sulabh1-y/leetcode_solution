a166c8ce2d85159cd1114bfbb4507b575b76fc4a
sulabh kumar sharma <126059586+sulabh1-y@users.noreply.github.com>
2026-08-11 05:04:38 +0530

    Time: 4 ms (10.7%), Space: 28.4 MB (29.38%) - LeetHub

---
+++ 2974-minimum-number-game/2974-minimum-number-game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> numberGame(vector<int>& nums) 
+    {
+        int min=0;
+        int n=nums.size();
+        vector<int> arr;
+        int alice,bob;
+        sort(nums.begin(),nums.end());
+        for(int i=0;i<n-1;i+=2)
+        {
+            alice=nums[i];
+            bob=nums[i+1];
+            arr.push_back(bob);
+            arr.push_back(alice);
+        }
+        return arr;
+    }
+};
