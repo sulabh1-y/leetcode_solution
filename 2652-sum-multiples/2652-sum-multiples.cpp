f036837f0b4fbf835ffef0a3fa77bc30bec15020
sulabh kumar sharma <126059586+sulabh1-y@users.noreply.github.com>
2026-08-11 05:48:07 +0530

    Time: 0 ms (100%), Space: 8.5 MB (49.32%) - LeetHub

--- 2652-sum-multiples/2652-sum-multiples.cpp
+++ 2652-sum-multiples/2652-sum-multiples.cpp
@@ -1,4 +1,4 @@
-class Solution {
+/*class Solution {
 public:
     int sumOfMultiples(int n) 
     {
@@ -24,6 +24,21 @@ public:
             if(i%3==0 && i%5==0 && i%7==0)
             {
                 sum-=i;
+            }
+        }
+        return sum;
+    }
+};*/
+class Solution {
+public:
+    int sumOfMultiples(int n) 
+    {
+        int sum=0;
+        for(int i=1;i<=n;i++)
+        {
+            if(i%3==0 || i%5==0 || i%7==0)
+            {
+                sum+=i;
             }
         }
         return sum;
